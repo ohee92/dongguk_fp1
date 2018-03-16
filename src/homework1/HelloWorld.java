317eb43ff541338c62a5f1cc8fa97fd01ed64561
ohee92 <37091363+ohee92@users.noreply.github.com>
2018-03-16 19:16:48 +0900

    Update HelloWorld.java

--- src/homework1/HelloWorld.java
+++ src/homework1/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String...args){
         System.out.println("Hello, World!");
-        System.out.println("Hello, java!!");
+        System.out.println("Hello, Java!!");
     }
 }
